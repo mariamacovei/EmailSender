7cbcea2113d8cf069d39bf3843fabcf1f6f70ed8
Maria <mariamacovei>
2021-03-13 21:53:08 +0200

    test

--- src/main/java/com/company/builder/Email.java
+++ src/main/java/com/company/builder/Email.java
@@ -1,24 +1,31 @@
 package com.company.builder;
 
 public class Email {
+    private int id;
     private String to;
     private String from;
     private String subject;
     private String content;
-    private String cc;
-    private String bcc;
 
 
     public Email() {
-
     }
 
-    public Email(String to, String from, String subject, String content) {
+    public Email(String to,String from, String subject, String content) {
         this.to = to;
         this.from = from;
         this.subject = subject;
         this.content = content;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTo() {
         return to;
     }
@@ -51,62 +58,40 @@ public class Email {
         this.content = content;
     }
 
-    public String getCc() {
-        return cc;
-    }
 
-    public void setCc(String cc) {
-        this.cc = cc;
-    }
 
-    public String getBcc() {
-        return bcc;
-    }
 
-    public void setBcc(String bcc) {
-        this.bcc = bcc;
-    }
-
-    @Override
-    public String toString() {
-        return "Email{" +
-                "to='" + to + '\'' +
-                ", from='" + from + '\'' +
-                ", subject='" + subject + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-    public static EmailBuilder emailBuilder() {
-        return new EmailBuilder();
-    }
-
-    public static class EmailBuilder {
-        private String to;
-        private String from;
-        private String subject;
-        private String content;
-
-        public EmailBuilder setTo(String to) {
-            this.to = to;
-            return this;
-        }
-
-        public EmailBuilder setFrom(String from) {
-            this.from = from;
-            return this;
-        }
-
-        public EmailBuilder setSubject(String subject) {
-            this.subject = subject;
-            return this;
-        }
-
-        public EmailBuilder setContent(String content) {
-            this.content = content;
-            return this;
-        }
-        public Email build() {
-            return new Email(to,from,subject,content);
-        }
-    }
+//    public static EmailBuilder emailBuilder() {
+//        return new EmailBuilder();
+//    }
+//
+//    public static class EmailBuilder {
+//        private String from;
+//        private String to;
+//        private String subject;
+//        private String content;
+//
+//        public void setFrom(String from) {
+//            this.from = from;
+//        }
+//
+//        public EmailBuilder setTo(String to) {
+//            this.to = to;
+//            return this;
+//        }
+//
+//        public EmailBuilder setSubject(String subject) {
+//            this.subject = subject;
+//            return this;
+//        }
+//
+//        public EmailBuilder setContent(String content) {
+//            this.content = content;
+//            return this;
+//        }
+//
+//        public Email build() {
+//            return new Email(to, from, subject, content);
+//        }
+//    }
 }
